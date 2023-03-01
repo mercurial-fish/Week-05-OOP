b38a00ae2dc833e4df7be71c5326c0548ffe72c5
mercurial-fish <dezi.a.cruz20@gmail.com>
2023-03-01 15:41:33 -0600

    Finished product. May revise later.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Book {
-    constructor(name, author, numberOfPages) {
+    constructor(bookName, author, numberOfPages) {
         this.bookName = bookName;
         this.author = author;
         this.numberOfPages = numberOfPages;
@@ -11,13 +11,13 @@ class Book {
 };
 
 class Genre {
-    constructor(name) {
+    constructor(genreName) {
         this.genreName = genreName;
         this.books = [];
     }
     addBook(bookName) {
         if (bookName instanceof Book) {
-            this.genre.push(bookName);
+            this.books.push(bookName);
         } else {
             throw new Error(`You can only add an instance of a book`);
             //this is in here as a way to handle exceptions and prevents code from crashing
@@ -51,9 +51,6 @@ class ReadingListMenu {
                 case "4":
                     this.displayGenres();
                     break;
-                case "5":
-                    this.displayAllBooks();
-                    break;
                 default:
                     readingSelection = 0;
 
@@ -74,37 +71,87 @@ class ReadingListMenu {
         1) Add Genre
         2) View Genre
         3) Remove Genre
-        4) Display All Genres
-        5) Display All Books`);
+        4) Display All Genres`);
     }
 
     showGenreMenuOptions (genreInfo) {
         return prompt (`
         0) back
-        2) Add Book
-        3) Remove Book
+        1) Add Book
+        2) Remove Book
         ----------------------
         ${genreInfo}`
         );
     }
 
     addGenre() {
-        let genre = prompt("Enter name for genre you want to add:");
-        this.genres.push(new Genre(genre));
-        console.log(genre);
-        console.log("Printing from addGenre method", this.genres)
-    }
+        let name = prompt("Enter name for genre you want to add:");
+        this.genres.push(new Genre(name));
+        alert("Great choice!");
+    };
 
     viewGenre() {
+        alert("Hmmm which one will you choose today? Let's see shall we?!");
         let genreIndex = prompt ("Enter the index of the genre you wish to view:");
         if (genreIndex > -1 && genreIndex < this.genres.length) {
             this.selectedGenre = this.genres[genreIndex];
-            let description = this.selectedGenre.name + "\n";
+            let description = this.selectedGenre.genreName + "\n";
 
             for (let i = 0; i < this.selectedGenre.books.length; i++) {
                 description += i + ') ' + '"' + this.selectedGenre.books[i].bookName + '" - ' +  
-                this.selectedGenre.books[i].author + "\n";
+                this.selectedGenre.books[i].author + ", " + this.selectedGenre.books[i].numberOfPages + " pages" + "\n";
+               
             }
+
+            let genreSelection = this.showGenreMenuOptions(description);
+            switch (genreSelection) {
+                case "1":
+                    this.nameBook();
+                    break;
+                case "2":
+                    this.removeBook();
+                    break;
+
+            };
+            
         }
+
+    };
+    removeGenre() {
+        let genreIndex = prompt("Enter the index of the genre you wish to remove:");
+        if (genreIndex > -1 && genreIndex < this.genres.length) {
+            this.genres.splice(genreIndex, 1);
+        }
+        alert("No, it's fine. *wipes away tears* I didn't even like that one anyway.");
+    };
+    displayGenres() {
+        alert("Prepare to be dazzled!");
+        alert("Did you do it? Did you prepare? Ok good. Now I'll show you.");
+        let genreString = "";
+        for (let i = 0; i < this.genres.length; i++) {
+            genreString += i + ") " + this.genres[i].genreName + "\n";
+           
+        }
+
+        alert(genreString);
+    };
+
+    nameBook() {
+        let bookName = prompt("Enter the title of the book you want to add:");
+        let author = prompt("Enter the name of the author:");
+        let numberOfPages = prompt("Enter the number of pages in the book:");
+        this.selectedGenre.books.push(new Book(bookName, author, numberOfPages));
+        alert("Ooooh! I love that one!");
+    };
+
+    removeBook() {
+        let bookIndex = prompt("Enter the index of the book you wish to remove:");
+        if (bookIndex > -1 && bookIndex < this.selectedGenre.books.length) {
+            this.selectedGenre.books.splice(bookIndex, 1);
+        }
+        alert("Awww man! But why?! Well... if you're sure...");
     }
 }
+
+let readingListMenu = new ReadingListMenu();
+readingListMenu.start();
